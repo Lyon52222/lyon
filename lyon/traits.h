3fb295fe2daa201bacadffc90447972b8bdb0fce
lyon <1765813715@qq.com>
2022-06-06 12:35:00 +0000

    add function traits

---
+++ lyon/traits.h
@@ -0,0 +1,49 @@
+#ifndef __LYON_TRAITS_H__
+#define __LYON_TRAITS_H__
+
+#include <functional>
+namespace lyon {
+template <class F> struct function_traits;
+
+// 普通函数
+template <class Ret, class... Args> struct function_traits<Ret(Args...)> {
+    enum { arity = sizeof...(Args) };
+    typedef Ret function_type(Args...);
+    typedef Ret return_type;
+    using stl_function_type = std::function<function_type>;
+    typedef Ret (*pointer)(Args...);
+
+    template <size_t I> struct args {
+        static_assert(I < arity,
+                      "index is out of range, inde must less than sizeof Args");
+
+        using type = typename std::tuple_element<I, std::tuple<Args...>>::type;
+    };
+};
+
+// 函数指针
+template <class Ret, class... Args>
+struct function_traits<Ret (*)(Args...)> : function_traits<Ret(Args...)> {};
+
+// std::function
+template <class Ret, class... Args>
+struct function_traits<std::function<Ret(Args...)>>
+    : function_traits<Ret(Args...)> {};
+
+// member function
+#define FUNCION_TRAITS(...)                                                    \
+    template <class ReturnType, class ClassType, class... Args>                \
+    struct function_traits<ReturnType (ClassType::*)(Args...) __VA_ARGS__>     \
+        : function_traits<ReturnType(Args...)> {};
+
+FUNCION_TRAITS()
+FUNCION_TRAITS(const)
+FUNCION_TRAITS(volatile)
+FUNCION_TRAITS(const volatile)
+
+// 函数对象
+template <class Callable>
+struct function_traits : function_traits<decltype(&Callable::operator())> {};
+
+} // namespace lyon
+#endif
---
+++ tests/test_traits.cc
@@ -0,0 +1,34 @@
+#include "lyon/traits.h"
+#include <functional>
+#include <iostream>
+
+template <class T> void PrintType() {
+    std::cout << typeid(T).name() << std::endl;
+}
+
+void test_function_traits() {
+    std::function<int(int)> f = [](int a) { return a; };
+
+    // 打印函数类型
+    PrintType<
+        lyon::function_traits<std::function<int(int, int)>>::function_type>();
+
+    //打印函数的参数个数
+    std::cout << lyon::function_traits<std::function<int(int, float)>>::arity
+              << std::endl;
+
+    // 打印函数的第一个参数类型
+    PrintType<
+        lyon::function_traits<std::function<int(int, float)>>::args<1>::type>();
+
+    //打印函数返回值类型
+    PrintType<lyon::function_traits<decltype(f)>::return_type>();
+
+    //打印函数指针类型
+    PrintType<lyon::function_traits<decltype(f)>::pointer>();
+}
+
+int main(int argc, char *argv[]) {
+    test_function_traits();
+    return 0;
+}
